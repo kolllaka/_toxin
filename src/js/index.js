adb485b8bc9bc4299cf80a4f7f4a7ffb9edd13ad
Denis <kotshredengera@gmail.com>
2023-06-16 13:23:52 +0300

    regroup project

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+// Custom Scripts
+@@include('libs/datapicker.js')
+@@include('libs/select.js')
+@@include('pages/index.js')
--- src/js/scripts.js
+++
@@ -1,4 +0,0 @@
-// Custom Scripts
-@@include('datapicker.js')
-@@include('select.js')
-@@include('main.js')
